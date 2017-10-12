4c601eaf60c6a3eac11480f59aa325a35c621579
mariomaldonado007 <mariomaldonado007@hotmail.com>
2017-10-11 23:29:34 -0500

    subimos silabario

---
+++ palabras japones.py
@@ -0,0 +1,10 @@
+# importar libreria random
+import random
+
+
+#vamos a crear una lista con silabas silabario hiragana
+#Generar un  numero aleatorio que sea un entero
+
+hiragana = ["a", "i", "u", "e", "o"]
+indice = random. randint(0,len(hiragana)-1)
+print (hiragana [indice])
